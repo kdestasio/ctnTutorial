65c6c02f20f2d6f4441e61c24c7759a2d3a04242
kdestasio <kdesyasio@gmail.com>
2021-12-16 13:41:35 -0800

    Make variable names lower_case_with_underscores as per PEP 8 style guide.

--- bidsQC/conversion/config_dcm2bids_batch.py
+++ bidsQC/conversion/config_dcm2bids_batch.py
@@ -9,16 +9,16 @@ group = "sanlab"
 # Set directories
 # These variables are used in the main script and need to be defined here.
 # They need to exist prior to running the script
-pathToStudyFolder = os.path.join(os.sep, "projects", "sanlab", "shared", "ctnTutorial") # folder that contains pathToBidsFolder and pathToConversionFolder
-pathToDicomFolder = os.path.join(os.sep, "projects", "sanlab", "shared", "REV_examples")
-pathToConversionFolder = os.path.join(pathToStudyFolder, "bidsQC", "conversion")  # Contains subject_list.txt, config file, and dcm2bids_batch.py
-pathToConfigFile = os.path.join(pathToConversionFolder, "study_config.json")  # path to and name of config file
+path_toplevel = os.path.join(os.sep, "projects", "sanlab", "shared", "ctnTutorial") # folder that contains path_bidsdata and path_conversionfolder
+path_dicoms = os.path.join(os.sep, "projects", "sanlab", "shared", "REV_examples")
+path_conversionfolder = os.path.join(path_toplevel, "bidsQC", "conversion")  # Contains subject_list.txt, config file, and dcm2bids_batch.py
+path_config = os.path.join(path_conversionfolder, "study_config.json")  # path to and name of config file
 singularity_image = os.path.join(os.sep, "projects", "sanlab", "shared", "containers", "dcm2bids_latest_2021-12-08.sif") # Set equal to "NA" if you are running the script locally
 
 # These variables are also used in the main script and need to be defined here.
 # If they don't exist, they will be created by the script
-pathToBidsFolder = os.path.join(os.sep, "projects", group, "shared", "ctnTutorial", "bids_data") # Where the niftis will be put
-logdir = os.path.join(pathToBidsFolder, "logs_dcm2bids")
+path_bidsdata = os.path.join(os.sep, "projects", group, "shared", "ctnTutorial", "bids_data") # Where the niftis will be put
+logdir = os.path.join(path_bidsdata, "logs_dcm2bids")
 outputlog = os.path.join(logdir, "outputlog_dcmn2bids" + datetime.now().strftime("%Y%m%d-%H%M") + ".txt")
 errorlog = os.path.join(logdir, "errorlog_dcm2bids" + datetime.now().strftime("%Y%m%d-%H%M") + ".txt")
 
